6d51cbcd17d8488bbc4d08d84fcf8d08874c389d
aaryavlal <aaryavlal12@gmail.com>
2024-10-18 14:18:46 -0700

    Update GameLevelWater.js

--- assets/js/rpg0x/GameLevelWater.js
+++ assets/js/rpg0x/GameLevelWater.js
@@ -44,20 +44,20 @@ class GameLevelWater {
     };
 
     // Player 2 sprite data (fish)
-    const sprite_src_fish = path + "/images/rpg/fishies.png";
-    const sprite_data_fish = {
+    const sprite_src_skeleton = path + "/images/rpg/skeleton.png";
+    const sprite_data_skeleton = {
         name: 'fish',
-        src: sprite_src_fish,
-        SCALE_FACTOR: 16,
+        src: sprite_src_skeleton,
+        SCALE_FACTOR: 20,
         STEP_FACTOR: 400,
         ANIMATION_RATE: 50,
-        pixels: {height: 256, width: 384},
+        pixels: {height: 129, width: 240},
         INIT_POSITION: { x: 0, y: 0 },
-        orientation: {rows: 8, columns: 12 },
-        down: {row: 0, start: 0, columns: 3 },  // 1st row
-        left: {row: 1, start: 0, columns: 3 },  // 2nd row
+        orientation: {rows: 4, columns: 3 },
+        down: {row: 3, start: 0, columns: 3 },  // 1st row
+        left: {row: 4, start: 0, columns: 3 },  // 2nd row
         right: {row: 2, start: 0, columns: 3 }, // 3rd row
-        up: {row: 3, start: 0, columns: 3 },    // 4th row
+        up: {row: 1, start: 0, columns: 3 },    // 4th row
     };
 
     // NPC sprite data (frog)
